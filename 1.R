e665f3be89df1de83024e60660879706ef9db1c7
Scott Shang <zezhou@ucsb.edu>
2022-04-03 21:56:37 -0700

    3rd

--- 1.R
+++ 1.R
@@ -1,2 +1,6 @@
-a = 1
-b = 2
+#Question 1:
+#Supervised learning: Machine learning that including prediction, estimation, model selection, and inference.
+#To be specific, supervised learning can accurately predict future response given predictors, understand how predictors affect response, select the best model, and assess the quality of predictions and estimation.
+#Unsupervised learning don't have a supervisor, and the model work on its own. (From Lec1 pg30)
+
+#Question 2:
